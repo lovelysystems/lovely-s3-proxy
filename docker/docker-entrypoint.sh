7482850d0cdf12a9890029fa520490e777f0b099
Lukas-Kaufmann <kaufmann@lovelysystems.com>
2025-03-26 19:18:09 +0100

    protocoll env

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,6 +4,7 @@ export LOG_LEVEL=${LOG_LEVEL:-"warn"}
 export PORT=${PORT:-'80'}
 export MGMT_PORT=${MGMT_PORT:-'10083'}
 export RESOLVER_ADDRESS=${RESOLVER_ADDRESS:-'127.0.0.11'}  # defaults to docker internal DNS
+export S3_PROTOCOL=${S3_PROTOCOL:-'http'}
 
 curr_dir="$(dirname "$0")"
 "$curr_dir/replace-env.sh"
