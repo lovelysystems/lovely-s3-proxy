42c2ea3347318df2b05c0597a9f4d77a9ac365a8
Lukas-Kaufmann <kaufmann@lovelysystems.com>
2025-03-27 14:02:59 +0100

    add S3_UPSTREAM

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,6 +5,7 @@ export PORT=${PORT:-'80'}
 export MGMT_PORT=${MGMT_PORT:-'10083'}
 export RESOLVER_ADDRESS=${RESOLVER_ADDRESS:-'127.0.0.11'}  # defaults to docker internal DNS
 export S3_PROTOCOL=${S3_PROTOCOL:-'https'}
+export S3_UPSTREAM=${S3_UPSTREAM:-"${S3_ENDPOINT}:443"}
 
 curr_dir="$(dirname "$0")"
 "$curr_dir/replace-env.sh"
