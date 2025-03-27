f3c615707dc9a82cdd240ae8e90af4d86311ba89
Lukas-Kaufmann <kaufmann@lovelysystems.com>
2025-03-27 09:34:33 +0100

    default to https

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,7 +4,7 @@ export LOG_LEVEL=${LOG_LEVEL:-"info"}
 export PORT=${PORT:-'80'}
 export MGMT_PORT=${MGMT_PORT:-'10083'}
 export RESOLVER_ADDRESS=${RESOLVER_ADDRESS:-'127.0.0.11'}  # defaults to docker internal DNS
-export S3_PROTOCOL=${S3_PROTOCOL:-'http'}
+export S3_PROTOCOL=${S3_PROTOCOL:-'https'}
 
 curr_dir="$(dirname "$0")"
 "$curr_dir/replace-env.sh"
